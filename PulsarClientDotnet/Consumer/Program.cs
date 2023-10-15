8ccc275e7a7b31a45ddae173ca68e8e169d22da8
Vladimir Shchur <odindafna2006@rambler.ru>
2023-10-15 10:32:12 -0700

    Added pulsar-client-dotnet

---
+++ PulsarClientDotnet/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Pulsar.Client.Api;
+
+const string serviceUrl = "pulsar://127.0.0.1:6650";
+const string subscriptionName = "my-subscription";
+var topicName = $"my-topic-{DateTime.Now.Ticks}";
+
+var client = await new PulsarClientBuilder()
+    .ServiceUrl(serviceUrl)
+    .BuildAsync();
+
+var consumer = await client.NewConsumer()
+    .Topic(topicName)
+    .SubscriptionName(subscriptionName)
+    .SubscribeAsync();
+
+var n = 10000000;
+
+// warmup
+for (var i = 0; i < n / 10; i++)
+{
+    var message = await consumer.ReceiveAsync();
+    await consumer.AcknowledgeAsync(message.MessageId);
+}
+
+var sw = new Stopwatch();
+sw.Start();
+for (var i = 0; i < n; i++)
+{
+    var message = await consumer.ReceiveAsync();
+    await consumer.AcknowledgeAsync(message.MessageId);
+
+    if (i % 100000 == 0)
+        Console.WriteLine($"Received {i} messages");
+}
+
+sw.Stop();
+Console.WriteLine(
+    $"Received {n} messages in {sw.ElapsedMilliseconds}ms. Speed: {n / (sw.ElapsedMilliseconds)}K msg/s");
---
+++ PulsarClientDotnet/Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Pulsar.Client.Api;
+
+const string serviceUrl = "pulsar://127.0.0.1:6650";
+var topicName = $"my-topic-{DateTime.Now.Ticks}";
+
+var client = await new PulsarClientBuilder()
+    .ServiceUrl(serviceUrl)
+    .BuildAsync();
+
+var producer =
+    await client.NewProducer()
+        .Topic(topicName)
+        .BlockIfQueueFull(true)
+        .CreateAsync();
+
+var n = 10000000;
+var bytes = new byte[750];
+
+// warmup
+for (var i = 0; i < n / 10; i++)
+{
+    await producer.SendAndForgetAsync(bytes);
+}
+
+var sw = new Stopwatch();
+sw.Start();
+for (var i = 0; i < n; i++)
+{
+    await producer.SendAndForgetAsync(bytes);
+
+    if (i % 100000 == 0)
+        Console.WriteLine($"Sent {i} messages");
+}
+
+sw.Stop();
+Console.WriteLine(
+    $"Sent {n} messages in {sw.ElapsedMilliseconds}ms. Speed: {n / (sw.ElapsedMilliseconds)}K msg/s");
